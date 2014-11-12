b3aef1038cee88f1a3ea2425a8b49362fdbd9193
Jeff Barczewski <jeff.barczewski@gmail.com>
2014-11-11 20:37:11 -0600

    add SIGTERM listener which triggers graceful shutdown

--- cluster-master.js
+++ cluster-master.js
@@ -534,6 +534,7 @@ function setupSignals () {
   try {
     process.on("SIGHUP", emitAndRestart)
     process.on("SIGINT", emitAndQuit)
+    process.on("SIGTERM", emitAndQuit)
     process.on("SIGABRT", emitAndQuitHard)
   } catch (e) {
     // Must be on Windows, waaa-waaah.
